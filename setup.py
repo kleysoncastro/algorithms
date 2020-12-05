59312944ef69cc2c2b1499c3c04c8ed287de126a
kleyson castro <kleysoncastro99@gmail.com>
2020-12-05 13:02:46 -0400

    Configuration file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='Algorithms',
+      version='0.1',
+      description='Algorithms and struct data',
+      url='https://github.com/kleysoncastro/algorithms-python',
+      author='kleyson castro',
+      author_email='kleysoncastro99@gamill.com',
+      license='MIT',
+      packages=['calc'],
+      zip_safe=False)
